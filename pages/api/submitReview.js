3e88bd08160b285a7748b7ceb7b8b44a12f4f94d
Daniel Adonis <bliam9145@gmail.com>
2024-10-01 12:32:19 +0100

    Add some modifications to the API

--- pages/api/submitReview.js
+++ pages/api/submitReview.js
@@ -1,7 +1,23 @@
-export default function handler(req, res) {
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
+export default async function (req, res) {
   if (req.method === "POST") {
-    const { projectId, rating, feedback } = req.body;
-    res.status(200).json({ message: "Review submitted!" });
+    try {
+      const { projectId, rating, feedback } = req.body;
+      console.log("Starting to process request ...............");
+      await addDoc(collection(db, "reviews"), {
+        projectId,
+        rating,
+        feedback,
+        createdAt: new Date(),
+      });
+      console.log("Review submitted successfully");
+      res.status(200).json({ message: "Review submitted successfully" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Failed to subnit review", error: error.message });
+    }
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
   }
