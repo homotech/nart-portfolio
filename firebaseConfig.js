dc795100df8a5c2493b89b4b50e0d7da718068f0
homotech <bliam9145@gmail.com>
2024-10-02 05:10:13 +0100

    Add the POST and the GET Api Routes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAShizUccdM8EjrW1OwEfFmHYEeu9YrEiA",
+  authDomain: "nart-portfolio.firebaseapp.com",
+  projectId: "nart-portfolio",
+  storageBucket: "nart-portfolio.appspot.com",
+  messagingSenderId: "266216173999",
+  appId: "1:266216173999:web:ed21a2e1ff28ed4ca47f5a",
 };
 
 const app = initializeApp(firebaseConfig);
