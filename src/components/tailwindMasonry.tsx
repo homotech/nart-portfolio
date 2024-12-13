c47e3d221ed697c3617cadb220de28569a207bb9
Daniel Adonis <bliam9145@gmail.com>
2024-12-13 01:04:36 +0100

    More work on the masonry grid

--- src/components/tailwindMasonry.tsx
+++ src/components/tailwindMasonry.tsx
@@ -14,56 +14,56 @@ const Images = [
     id: 1,
     image: ImageOne,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 2,
     image: ImageTwo,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 3,
     image: ImageThree,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 4,
     image: ImageFour,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 5,
     image: ImageFive,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 6,
     image: ImageSix,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 7,
     image: ImageSeven,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
   {
     id: 8,
     image: ImageEight,
     alt: "",
-    projectName: "Project Name",
+    projectDesc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
     projectHeader: "Project Head",
   },
 ];
@@ -72,19 +72,27 @@ const TailwindMasonry = () => {
   return (
     <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 mb-4">
       {Images.map((item) => (
-        <div className="mb-4 group overflow-hidden">
+        <div className="mb-4 group overflow-hidden relative rounded-lg ">
           <Link href="/" key={item.id}>
             <Image
               src={item.image}
               alt={item.alt}
-              className="w-full rounded-lg mb-2 transition-transform duration-300 group-hover:scale-110"
+              className="w-full transition-transform duration-300 group-hover:scale-110"
             ></Image>
           </Link>
-          <div className="absolute ">
-            <p className="font-br-firma-semibold text-lg">
+          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 transform-opacity group-hover:opacity-100 text-white duration-300 flex flex-col justify-center items-center ">
+            <p className="font-br-firma-semibold text-3xl">
               {item.projectHeader}
             </p>
-            <p className="font-br-firma-semibold text-sm">{item.projectName}</p>
+            <p className="font-br-firma-semibold text-center w-2/3 text-sm my-2">
+              {item.projectDesc}
+            </p>
+            <Link
+              href="/"
+              className="px-4 py-2 bg-white text-black font-br-firma-regular text-base rounded-lg"
+            >
+              View Project
+            </Link>
           </div>
         </div>
       ))}
