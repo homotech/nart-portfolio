78ad8b475a58bd78624e509f982da1fb0bd5c875
homotech <bliam9145@gmail.com>
2024-12-24 17:11:03 +0100

    Fix build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-import config from "./config.json" assert { type: "json" };
 
-const nextConfig = {
-  env: {
-    NEXTAUTH_URL: config.nextAuthUrl,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
