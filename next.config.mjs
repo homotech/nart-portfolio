c6403fd8eca7fdbcaa68e32b782368c84eccf7f4
homotech <bliam9145@gmail.com>
2025-06-09 14:57:44 +0100

    Update site with latest changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,6 @@ const nextConfig = {
   basePath: isProd ? "/nart-portfolio" : "",
   assetPrefix: isProd ? "/nart-portfolio/" : "",
   images: { unoptimized: true },
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/info": { page: "/info" },
-      "/portfolio": { page: "/portfolio" },
-      "/news": { page: "/news" },
-    };
-  },
 };
 
 export default nextConfig;
