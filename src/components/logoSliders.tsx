52077834a120a7a7b30a0c215d8453454909331f
homotech <bliam9145@gmail.com>
2025-06-11 04:53:06 +0100

    Add some images

--- src/components/logoSliders.tsx
+++ src/components/logoSliders.tsx
@@ -2,6 +2,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import BOAA from "@/public/Logos/boaa.svg";
+import FiBeauty from "@/public/Logos/fi-beauty.svg";
+import Omoeta from "@/public/Logos/omoeta.svg";
+import Payaway from "@/public/Logos/payaway.svg";
+import Tiqeasy from "@/public/Logos/tiqeasy.svg";
 import Image from "next/image";
 import { Autoplay } from "swiper/modules";
 const logos = [
@@ -10,41 +14,43 @@ const logos = [
     src: BOAA,
   },
   {
-    name: "Logo 1",
-    src: BOAA,
+    name: "Logo 2",
+    src: FiBeauty,
   },
   {
-    name: "Logo 1",
-    src: BOAA,
+    name: "Logo 3",
+    src: Omoeta,
   },
   {
-    name: "Logo 1",
-    src: BOAA,
+    name: "Logo 4",
+    src: Payaway,
+  },
+  {
+    name: "Logo 5",
+    src: Tiqeasy,
   },
 ];
 const LogoSliders = () => {
   return (
     <Swiper
-      spaceBetween={16}
-      slidesPerView={2.5}
+      spaceBetween={1}
+      slidesPerView={2}
       loop={true}
       autoplay={{ delay: 1500, disableOnInteraction: false }}
       modules={[Autoplay]}
+      speed={800}
     >
       {logos.map((logo, index) => (
         <SwiperSlide key={index}>
-          {/* <img
-            src={logo.src}
-            alt={logo.name}
-            className="w-28 h-28 object-contain rounded-md border mx-auto"
-          /> */}
-          <Image
-            src={logo.src}
-            alt={logo.name}
-            className="w-28 h-28 object-contain rounded-md border mx-auto"
-            width={112}
-            height={112}
-          />
+          <div className="flex h-24 justify-center items-center">
+            <Image
+              src={logo.src}
+              alt={logo.name}
+              className="object-contain"
+              width={100}
+              height={100}
+            />
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>
