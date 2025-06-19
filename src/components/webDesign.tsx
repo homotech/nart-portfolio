8e60a9c836bc6e2dab0e044ab9211c351038eb05
homotech <bliam9145@gmail.com>
2025-06-19 05:09:28 +0100

    Adding The buttons on the web development projects

--- src/components/webDesign.tsx
+++ src/components/webDesign.tsx
@@ -1,6 +1,7 @@
 // "use client";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import WebProjects from "@/src/data/webprojects";
 import Link from "next/link";
 
 const WebDesign = () => {
@@ -11,29 +12,28 @@ const WebDesign = () => {
       </h1>
       <div className="">
         <ul>
-          <li className="flex items-center">
-            <Link
-              href="/"
-              className="text-xl tracking-tighter font-br-firma-medium hover:text-gray-500 flex-grow"
-            >
-              Quote App
-            </Link>
-            {/* <FontAwesomeIcon width={24} height={24} icon={faChevronRight} /> */}
-            <div className="flex gap-4">
-              <Link
-                href="https://homotech.github.io/quote-app/"
-                className="text-sm px-4 py-2 hover:bg-black hover:text-white font-br-firma-regular track-tighter bg-gray-100"
-              >
-                Live Demo
-              </Link>
-              <Link
-                href="https://github.com/homotech/quote-app.git"
-                className="text-sm px-4 py-2 hover:bg-black hover:text-white font-br-firma-regular track-tighter bg-gray-100"
-              >
-                Github Repo
-              </Link>
-            </div>
-          </li>
+          {WebProjects.map((project) => (
+            <li className="flex items-center" key={project.id}>
+              <p className="text-xl tracking-tighter font-br-firma-medium flex-grow">
+                {project.projectName}
+              </p>
+              {/* <FontAwesomeIcon width={24} height={24} icon={faChevronRight} /> */}
+              <div className="flex gap-4">
+                <Link
+                  href={project.liveDemo}
+                  className="text-sm px-4 py-2 hover:bg-black hover:text-white font-br-firma-regular track-tighter bg-gray-100"
+                >
+                  Live Demo
+                </Link>
+                <Link
+                  href={project.githubRepo}
+                  className="text-sm px-4 py-2 hover:bg-black hover:text-white font-br-firma-regular track-tighter bg-gray-100"
+                >
+                  Github Repo
+                </Link>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/data/webprojects.ts
@@ -0,0 +1,9 @@
+const WebProjects = [
+  {
+    id: "1",
+    projectName: "Quote App",
+    liveDemo: "https://homotech.github.io/quote-app/",
+    githubRepo: "https://github.com/homotech/quote-app.git",
+  },
+];
+export default WebProjects;
